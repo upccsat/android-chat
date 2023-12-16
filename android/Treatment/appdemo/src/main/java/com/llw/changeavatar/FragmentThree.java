f7bc820d4b40e5d86ebf79e5555368ada96e2a1d
LiuBaiWan-Runner <liuzhicheng592@hotmail.com>
2023-12-16 10:44:54 +0800

    add personal home page and change avatar functions

---
+++ android/Treatment/appdemo/src/main/java/com/llw/changeavatar/FragmentThree.java
@@ -0,0 +1,19 @@
+package com.llw.changeavatar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentThree extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_three, container, false);
+    }
+}
